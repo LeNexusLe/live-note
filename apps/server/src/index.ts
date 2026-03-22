ecc71a6c8c0cff53245c59203307fc23ebe8b3c3
LeCarteloo <filippapiernik1999@gmail.com>
2026-03-22 14:03:52 +0100

    feat: Adjust structure

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono';
-import { HTTPException } from 'hono/http-exception';
-import type { ErrorResponse } from 'shared';
+
+import { errorHandler } from './middleware/error';
 
 export const app = new Hono();
 
@@ -8,21 +8,7 @@ app.get('/health', async c => {
   return c.json('ok', 200);
 });
 
-app.onError((error, context) => {
-  if (error instanceof HTTPException) {
-    return error.res ?? context.json<ErrorResponse>({ error: error.message }, error.status);
-  }
-
-  return context.json<ErrorResponse>(
-    {
-      error:
-        process.env.NODE_ENV === 'production'
-          ? 'Internal Server Error'
-          : (error.stack ?? error.message),
-    },
-    500,
-  );
-});
+app.onError(errorHandler);
 
 export default {
   port: 5000,
---
+++ apps/server/src/middleware/error.ts
@@ -0,0 +1,19 @@
+import { type ErrorHandler } from 'hono';
+import { HTTPException } from 'hono/http-exception';
+import type { ErrorResponse } from 'shared';
+
+export const errorHandler: ErrorHandler = (error, context) => {
+  if (error instanceof HTTPException) {
+    return error.res ?? context.json<ErrorResponse>({ error: error.message }, error.status);
+  }
+
+  return context.json<ErrorResponse>(
+    {
+      error:
+        process.env.NODE_ENV === 'production'
+          ? 'Internal Server Error'
+          : (error.stack ?? error.message),
+    },
+    500,
+  );
+};
